f7381dc135fc4302dc25424b0d15325a93f72b57
Michael Umenta <michael.umenta@vanderbilt.edu>
2016-12-15 17:10:21 -0600

    added header

---
+++ ios/NarroJob/NarroJob/JobClassifier.h
@@ -0,0 +1,13 @@
+//
+//  JobClassifier.h
+//  NarroJob
+//
+//  Created by Michael Umenta on 12/15/16.
+//  Copyright © 2016 Michael Umenta. All rights reserved.
+//
+
+#ifndef JobClassifier_h
+#define JobClassifier_h
+
+
+#endif /* JobClassifier_h */
